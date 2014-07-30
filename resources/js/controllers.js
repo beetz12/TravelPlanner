1c80de723733afd1faef6ecc7000085d928168a7
cm-pc <beetz12@gmail.com>
2014-07-29 22:35:14 -0400

    first milestone

--- resources/js/controllers.js
+++ resources/js/controllers.js
@@ -2,7 +2,7 @@
 
 /* Controllers */
 
-  myApp.controller('TravelDestCtrl', function ($scope, travelDest){
+  myApp.controller('DestListCtrl', function ($scope, $location, travelDest){
   	$scope.myData = travelDest.getList().then(function (destList)
       {
           $scope.travelDest = destList[0];
@@ -10,22 +10,21 @@
 
       $scope.gridOptions = { data: 'travelDest',
           columnDefs: [{ field: 'name', displayName: 'Name', width: "200px", resizable: false},
-              { field: 'description', displayName: 'Description', width: "*" }]};
+              { field: 'description', displayName: 'Description', width: "*" }],
+          multiSelect: false, selectedItems: []};
 
       $scope.EditClick = function (rows){
-        if(!rows){
-            console.log('at least 1 row must be selected');
-            console.log(rows);
-        }
-          else if(rows.length > 1){
-            console.log('only 1 row can be selected');
-            console.log(rows);
-        }
-          else{
-            alert('success');
-            console.log(rows);
-        }
-
+          console.log('edit clicked');
+          if(!rows || rows.length < 1){
+              alert('at least 1 row must be selected');
+          } else {
+              $location.path('/destination/' + rows[0].id);
+          }
       };
 
   });
+
+myApp.controller('DestEditCtrl', function($scope, $route){
+    $scope.dest = {"name": "canada", "description": "french speaking country"};
+    console.log($route.current.foo);
+});
