67c8e0ac80dddf9c97e60837226e69316274cbd0
cm-pc <beetz12@gmail.com>
2014-07-31 10:17:23 -0400

    final commit

--- resources/bower/angular-seed/app/js/filters.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* Filters */
-
-myApp.filter('interpolate', ['version', function(version) {
-    return function(text) {
-      return String(text).replace(/\%VERSION\%/mg, version);
-    };
-  }]);
