705ccffcc9f507f89f56cc2436b598b883bcc7a1
cm-pc <beetz12@gmail.com>
2014-07-31 02:18:34 -0400

    working at last!

--- resources/js/directives.js
+++ resources/js/directives.js
@@ -8,3 +8,22 @@ myApp.directive('appVersion', ['version', function(version) {
       elm.text(version);
     };
   }]);
+
+myApp.directive('resizable', function($window) {
+    return function($scope) {
+        $scope.initializeWindowSize = function() {
+            var wHeight = $window.innerHeight;
+            var result = 0;
+            if(wHeight > 433){
+                result = (wHeight - 383)*0.4;
+            }
+            console.log('result is: ' + result);
+            return $scope.topMargin = result + 'px';
+        };
+        $scope.initializeWindowSize();
+        return angular.element($window).bind('resize', function() {
+            $scope.initializeWindowSize();
+            return $scope.$apply();
+        });
+    };
+});
